e50ea90bc42bde8222c246fa57a8a22794041634
Dinindu98 <dinindu.ayagama@gmail.com>
2019-09-11 16:36:57 -0700

    first commit

---
+++ Area &    Perimeter      ( lesson 1) DININDU SANDARUWAN creation.py
@@ -0,0 +1,48 @@
+print("Welcome")
+print("We help you learn mathematics step by step easy way")
+print("1st lesson is Perimeter and Area")
+print("(this is valid for rectangle & square)")
+length=0
+width=0
+area=0
+perimeter=0
+
+
+length=int(input("first you enter a length :- "))
+width=int(input("now you enter a width :- "))
+area= length * width
+perimeter= 2*(length+width)
+
+print("Area = length * width")
+
+print("now you can see your rectangle area")
+
+print("Area = length * width")
+print( "Area = ",length,"*",width)
+print("Area = ",area)
+
+print("next we see how to find perimeter, It is easy . Follow these step")
+
+print("Perimeter = length + length + width + width")
+
+print("OR")
+
+print("Perimeter = 2 * ( length + width)")
+
+print("you can use one of this")
+
+print("Perimeter = length + length + width + width")        
+print("Perimeter = ",length," + ",length," + ",width," + ",width)
+print("Perimeter = ",perimeter)
+
+print("Perimeter = 2 * ( length + width)")
+print("Perimeter = 2 * ( ",length," + ",width ,")")
+print("Perimeter = ",perimeter)
+
+print("I think you learn well, see you again next mathematics lesson")
+
+print("            DININDU SANDARUWAN creation ")
+print("find me on facebook, e-maill - dinindu.aygama@gmail.com")
+
+dsmw = input()
+
